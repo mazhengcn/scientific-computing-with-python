1bfcb9f1881f0f025f00cb0d5ce660517386483e
Zheng Ma <mazhengcn@outlook.com>
2022-04-22 15:51:44 +0800

    feat: update

--- Chapter15/bisection.py
+++ Chapter15/bisection.py
@@ -1,4 +1,4 @@
-def bisect(f, a, b, tol=1.e-8):
+def bisect(f, a, b, tol=1.0e-8):
     """
     Implementation of the bisection algorithm
     f real valued function
@@ -6,14 +6,14 @@ def bisect(f, a, b, tol=1.e-8):
     f(a) * f(b) <= 0
     tol tolerance (float)
     """
-    if f(a) * f(b)> 0:
+    if f(a) * f(b) > 0:
         raise ValueError("Incorrect initial interval [a, b]")
     for i in range(100):
-        c = (a + b) / 2.
+        c = (a + b) / 2.0
         if f(a) * f(c) <= 0:
             b = c
         else:
             a = c
         if abs(a - b) < tol:
             return (a + b) / 2
-    raise Exception('No root found within the given tolerance {tol}')
+    raise Exception("No root found within the given tolerance {tol}")
